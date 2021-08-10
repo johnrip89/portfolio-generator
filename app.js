50f643e2d3755057eda5ce4dcad6ca472acbb1d2
johnrip89 <lebronjohn89@gmail.com>
2021-08-10 11:04:52 -0600

    added CSS and cleaned up look of code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
@@ -71,7 +71,7 @@ const promptProject = (portfolioData) => {
             message: 'Would you like to enter another project?',
             default: false
         }
-    ])        
+    ])
         .then(projectData => {
             portfolioData.projects.push(projectData);
             if (projectData.confirmAddProject) {
@@ -133,19 +133,47 @@ const promptUser = () => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-        fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
 
-        console.log('Page created! Check out index.html in this directory to see it!');
-        });
-    });
+  //**CALLBACK HELL**/
+//promptUser()
+//    .then(promptProject)
+//    .then(portfolioData => {
+//        const pageHTML = generatePage(portfolioData);
 
+//        fs.writeFile('./dist/index.html', pageHTML, err => {
+//            if (err) {
+//                console.log(err);
+//                return;
+//            }
+//            console.log('Page created! Check out index.html in this directory to see it!');
 
-
+//            fs.copyFile('./src/style.css', './dist/style.css', err => {
+//                if (err) {
+//                    console.log(err);
+//                    return;
+//                }
+//                console.log('Style sheet copied successfully!');
+//            });
+//        });
+//    });
 
 
 
